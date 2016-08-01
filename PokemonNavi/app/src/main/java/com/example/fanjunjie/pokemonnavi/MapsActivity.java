3251c87360aa1178dec83f6453d2f9f5ebc8c04b
REVANTHSAI <rsr361@nyu.edu>
2016-08-01 02:24:33 -0400

    Add button and current location button added

--- PokemonNavi/app/src/main/java/com/example/fanjunjie/pokemonnavi/MapsActivity.java
+++ PokemonNavi/app/src/main/java/com/example/fanjunjie/pokemonnavi/MapsActivity.java
@@ -1,8 +1,11 @@
 package com.example.fanjunjie.pokemonnavi;
 
 import android.app.Dialog;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -14,40 +17,50 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    GoogleMap googleMapvar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        if(GoogleServiceAvailability())
-        {
+        if (GoogleServiceAvailability()) {
             MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.fragment);
             mapFragment.getMapAsync(this);
         }
     }
 
 
-    public boolean GoogleServiceAvailability()
-    {
-        GoogleApiAvailability api=GoogleApiAvailability.getInstance();
-        int isAvailable=api.isGooglePlayServicesAvailable(this);
-        if(api.isGooglePlayServicesAvailable(this)== ConnectionResult.SUCCESS)
-        {
+    public boolean GoogleServiceAvailability() {
+        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
+        int isAvailable = api.isGooglePlayServicesAvailable(this);
+        if (api.isGooglePlayServicesAvailable(this) == ConnectionResult.SUCCESS) {
             return true;
-        }
-        else if (api.isUserResolvableError(isAvailable))
-        {
-            Dialog dialog=api.getErrorDialog(this,isAvailable,0);
+        } else if (api.isUserResolvableError(isAvailable)) {
+            Dialog dialog = api.getErrorDialog(this, isAvailable, 0);
             dialog.show();
-        }
-        else {
-            Toast.makeText(this,"cant connect to play services",Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "cant connect to play services", Toast.LENGTH_LONG).show();
         }
         return false;
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        googleMapvar = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+    }
+
+    public void setMapCameraToCurrent(View view) {
 
     }
 }

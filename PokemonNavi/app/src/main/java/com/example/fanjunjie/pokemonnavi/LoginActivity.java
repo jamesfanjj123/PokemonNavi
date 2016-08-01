9104fe5b210e5d7c664605ec069c178e9043d569
REVANTHSAI <rsr361@nyu.edu>
2016-08-01 02:27:21 -0400

    Buttons added

--- PokemonNavi/app/src/main/java/com/example/fanjunjie/pokemonnavi/LoginActivity.java
+++ PokemonNavi/app/src/main/java/com/example/fanjunjie/pokemonnavi/LoginActivity.java
@@ -1,15 +1,99 @@
 package com.example.fanjunjie.pokemonnavi;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
 
 
 public class LoginActivity extends AppCompatActivity {
 
+   // LoginButton loginButton;
+   CallbackManager callbackManager;
+   TextView textView;
+   AccessTokenTracker accessTokenTracker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
         setContentView(R.layout.activity_login);
+
+
+        textView= (TextView) findViewById(R.id.textView);
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+
+
+
+
+            }
+        };
+
+
+    }
+
+
+    public void FacebookLogin(View view) {
+
+
+        callbackManager=CallbackManager.Factory.create();
+
+       // LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+
+
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+               // Toast.makeText(LoginActivity.this,loginResult.toString(),Toast.LENGTH_LONG).show();
+                textView.setText("Hellllllooooo"+loginResult.getAccessToken().getToken().toString());
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        accessTokenTracker.stopTracking();
     }
 }

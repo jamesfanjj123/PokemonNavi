d50c22f8761c061560cc60f9a3c68232247692ce
REVANTHSAI <rsr361@nyu.edu>
2016-08-03 15:32:26 -0400

    No Location access permission handled

--- PokemonNavi/app/src/main/java/com/example/fanjunjie/pokemonnavi/MapsActivity.java
+++ PokemonNavi/app/src/main/java/com/example/fanjunjie/pokemonnavi/MapsActivity.java
@@ -1,6 +1,7 @@
 package com.example.fanjunjie.pokemonnavi;
 
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.IntentSender;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
@@ -8,6 +9,7 @@ import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -173,6 +175,18 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         Log.i(TAG, "Location services connected.");
 
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Location services not Available.");
+
+            AlertDialog alert = new AlertDialog.Builder(this).create();
+            alert.setTitle("Cant access your location");
+            alert.setMessage("Please turn on Location access permission");
+            alert.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alert.show();
 
             return;
         }

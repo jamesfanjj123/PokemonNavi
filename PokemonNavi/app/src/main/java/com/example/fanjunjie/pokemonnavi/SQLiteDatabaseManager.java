8947606da7dc14e014beca89d86acf2981bd821a
Fan Junjie <fanjunjie@Fans-MBP-2.fios-router.home>
2016-08-05 02:44:33 -0400

    PokeDataTable and Download service done

--- PokemonNavi/app/src/main/java/com/example/fanjunjie/pokemonnavi/SQLiteDatabaseManager.java
+++ PokemonNavi/app/src/main/java/com/example/fanjunjie/pokemonnavi/SQLiteDatabaseManager.java
@@ -1,21 +1,23 @@
 package com.example.fanjunjie.pokemonnavi;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
  * Created by fanjunjie on 8/3/16.
  */
-public class SQLiteDatabaseManager {
+public class SQLiteDatabaseManager extends SQLiteOpenHelper{
 
-    public class SqlHelper extends SQLiteOpenHelper{
+
 
         private static final String DATABASE_NAME = "PokeDB";
         private static final int DATABASE_VERSION = 1;
 
 
-        public SqlHelper(Context context) {
+        public SQLiteDatabaseManager(Context context) {
             super(context, DATABASE_NAME, null, DATABASE_VERSION);
 
         }
@@ -23,9 +25,9 @@ public class SQLiteDatabaseManager {
         @Override
         public void onCreate(SQLiteDatabase db) {
 
-            db.execSQL( "CREATE TABLE " + "PokeData" + "(" + "id" + " INTEGER ," + "name" + " VARCHAR(255)," + "picurl" + " VARCHAR(255)"+ ")");
-            db.execSQL("CREATE TABLE " + "UserData" + "(" + "id" + " VARCHAR(255) ," + "pokename" + " VARCHAR(255)," + "finddate" + " DATETIME"+ ")");
-            db.execSQL("CREATE TABLE " + "LocationData" + "(" + "name" + " VARCHAR(255),"+ "lon" + " VARCHAR(255)," + "lat" + " VARCHAR(255)"+ ")");
+            db.execSQL( "CREATE TABLE " + "PokeData" + "(" + "id" + " INTEGER PRIMARY KEY AUTOINCREMENT ," + "name" + " VARCHAR(255)," + "picurl" + " VARCHAR(255)"+ ")");
+          //  db.execSQL("CREATE TABLE " + "UserData" + "(" + "id" + " VARCHAR(255) ," + "pokename" + " VARCHAR(255)," + "finddate" + " DATETIME"+ ")");
+          //  db.execSQL("CREATE TABLE " + "LocationData" + "(" + "name" + " VARCHAR(255),"+ "lon" + " VARCHAR(255)," + "lat" + " VARCHAR(255)"+ ")");
 
         }
 
@@ -33,5 +35,19 @@ public class SQLiteDatabaseManager {
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
         }
+
+
+        public boolean insertPokeData(String name, String url){
+
+            SQLiteDatabase db = this.getWritableDatabase();
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("name",name);
+            contentValues.put("picurl",url);
+            db.insert("PokeData",null,contentValues);
+
+            return true;
+
+
+
+        }
     }
-}
